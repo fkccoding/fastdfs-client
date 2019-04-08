e25f8eb52c0a3b79d8f65f9ab6e8fc3716b3e076
fc <840880472@qq.com>
2019-04-08 11:47:37 +0800

    update code

--- src/main/java/com/kd/fastdfsclient/FastdfsClientApplication.java
+++ src/main/java/com/kd/fastdfsclient/FastdfsClientApplication.java
@@ -2,7 +2,9 @@ package com.kd.fastdfsclient;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 @MapperScan("com.kd.fastdfsclient.mapper")
