023ccb95501a7b2b3d14037d83f4901d592a0c67
fc <840880472@qq.com>
2019-04-19 19:46:50 +0800

    improve my code to work better with front end

--- src/main/java/com/kd/fastdfsclient/mapper/FileInfoMapper.java
+++ src/main/java/com/kd/fastdfsclient/mapper/FileInfoMapper.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.kd.fastdfsclient.entity.FileInfo;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
@@ -11,7 +12,7 @@ import java.util.List;
  * @Author: www.chuckfang.top
  * @Date: 2019/3/26 11:15
  */
-@Mapper
+@Repository
 public interface FileInfoMapper extends BaseMapper<FileInfo> {
 
     FileInfo findFileByName(String filename);
