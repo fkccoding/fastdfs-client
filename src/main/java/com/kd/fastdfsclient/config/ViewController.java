5d3f1f02debdf59537d593b2abe20d82cbc28f6b
fkccoding <fkc_jobs@163.com>
2019-07-01 15:18:34 +0800

    add serialVersionUID

--- src/main/java/com/kd/fastdfsclient/config/ViewController.java
+++ src/main/java/com/kd/fastdfsclient/config/ViewController.java
@@ -5,8 +5,8 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
- * @Author: www.chuckfang.top
- * @Date: 2019/1/3 14:51
+ * Author: www.chuckfang.top
+ * Date: 2019/1/3 14:51
  * 这里是配置一个简单页面给后端调试接口使用，发布版本可以去掉
  */
 @Configuration
